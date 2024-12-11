df08a11676236f9a5a21794364f6e0acbbf02c09
Umesh <umesh.nod@gmail.com>
2024-12-12 00:26:30 +0530

    cred added

--- database.py
+++ database.py
@@ -2,8 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+import os
+#postgresql://airobo_user:tcLr3o0k6KRqhdni3FdBZuxe8WaRUJjG@dpg-ctctk7i3esus73bmf9r0-a/airobo
 #sql_database_url="postgresql://<username>:<password>@<ip-address/hostname>/<database_name>"
-sql_database_url="postgresql://postgres:sahil@localhost:5433/fastapi"
+sql_database_url="postgresql://airobo_user:tcLr3o0k6KRqhdni3FdBZuxe8WaRUJjG@dpg-ctctk7i3esus73bmf9r0-a.oregon-postgres.render.com/airobo"
+
 
 engine = create_engine(sql_database_url)
 
